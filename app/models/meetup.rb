1531787639728117dbf9d2d031099c799306ba57
Uthaeus <romanlavery@gmail.com>
2023-05-10 20:11:31 -0600

    added validations in models, mounted uploaders

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,2 +1,6 @@
 class Meetup < ApplicationRecord
+    validates_presence_of :title, :description, :location, :time, :date 
+
+    mount_uploader :main_image, MeetupUploader
+    mount_uploader :thumb_image, MeetupUploader
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates_presence_of :title, :body 
+
+    mount_uploader :image, PostUploader
 end
